cac86638719ea7628dadbb77a981e4e0890d78e0
Cristian Betancourt <cristian.camilo.betancourt.rin@gmail.com>
2023-05-03 19:50:11 -0500

    First push lists complete

---
+++ Js/mascotas.js
@@ -0,0 +1,198 @@
+let tablaMascotas = document.getElementById("tabla-mascotas");
+let cuerpoTabla = tablaMascotas.querySelector("tbody"); //Consultar un hijo del selector llamado
+//console.log(cuerpoTabla);
+//!USANDO LISTAS
+let nombeMascotas = ["Luna", "Lucas", "Oreo"];
+let nombreDueno = ["Ana", "Daniel", "Juan"];
+let cedulaDueno = ["1234567", "51834967", "89526325"];
+let edadMascota = ["5 años", "1 año", "2 años"];
+let telDueno = ["3125679852", "3221456289", "3214567891"];
+let especialidad = [
+  "Veterinaria Conductual",
+  "Medicina General",
+  "Cirugía veterniaria",
+];
+let medico = ["Jorge", "Daniela", "Kevin"];
+
+for (let i = 0; i < nombeMascotas.length; i++) {
+  let fila = cuerpoTabla.insertRow(); //insertar fila TR . insertRow
+  //Insertar columna  TD .insertcell
+  let celdaNombreMascota = fila.insertCell();
+  let celdaNombreDueno = fila.insertCell();
+  let celdaCedulaDueno = fila.insertCell();
+  let celdaEdadMascota = fila.insertCell();
+  let celdaTelefonoDueno = fila.insertCell();
+  let celdaEspecialidad = fila.insertCell();
+  let celdaMedico = fila.insertCell();
+  //Rellenar contenidos con cada iteración
+  celdaNombreMascota.textContent = nombeMascotas[i];
+  celdaNombreDueno.textContent = nombreDueno[i];
+  celdaCedulaDueno.textContent = cedulaDueno[i];
+  celdaEdadMascota.textContent = edadMascota[i];
+  celdaTelefonoDueno.textContent = telDueno[i];
+  celdaEspecialidad.textContent = especialidad[i];
+  celdaMedico.textContent = medico[i];
+}
+
+//!USANDO OBJETOS
+let mascotas = [
+  {
+    nombreMascota: "Luna",
+    nombreDueño: "Ana",
+    cedulaDueño: "1234567",
+    edadMascota: "5 años",
+    telefonoDueño: "3105678946",
+    especialidad: "Veterinaria Conductual",
+    medico: "Jorge",
+  },
+  {
+    nombreMascota: "Orion",
+    nombreDueño: "Jorge",
+    cedulaDueño: "6789015",
+    edadMascota: "2 meses",
+    telefonoDueño: "12345678909",
+    especialidad: "Medicina interna",
+    medico: "Daniela",
+  },
+  {
+    nombreMascota: "Maily",
+    nombreDueño: "Kevin",
+    cedulaDueño: "5678954",
+    edadMascota: "2 años",
+    telefonoDueño: "5678902567",
+    especialidad: "Dermatología veterinaria",
+    medico: "Andres",
+  },
+  {
+    nombreMascota: "Mystic",
+    nombreDueño: "Nicolas",
+    cedulaDueño: "56723982",
+    edadMascota: "8 años",
+    telefonoDueño: "5678952567",
+    especialidad: "Farmacología veterinaria",
+    medico: "Jeisson",
+  },
+];
+for (let i = 0; i < mascotas.length; i++) {
+  let mascota = mascotas[i]; //Traemos cada objeto de cada posicion dentro del array y se almacenan en la variable mascota.
+  //console.log(mascota);
+  //Se crea la fila y columnas
+  let fila = cuerpoTabla.insertRow();
+  let celdaNombreMascota = fila.insertCell();
+  let celdaNombreDueño = fila.insertCell();
+  let celdaCedulaDueño = fila.insertCell();
+  let celdaEdadMascota = fila.insertCell();
+  let celdaTelefonoDueño = fila.insertCell();
+  let celdaEspecialidad = fila.insertCell();
+  let celdaMedico = fila.insertCell();
+  //Se rellenan las celdas con las propiedades de cada objeto sustraido con la variable mascota
+  celdaNombreMascota.textContent = mascota.nombreMascota;
+  celdaNombreDueño.textContent = mascota.nombreDueño;
+  celdaCedulaDueño.textContent = mascota.cedulaDueño;
+  celdaEdadMascota.textContent = mascota.edadMascota;
+  celdaTelefonoDueño.textContent = mascota.telefonoDueño;
+  celdaEspecialidad.textContent = mascota.especialidad;
+  celdaMedico.textContent = mascota.medico;
+}
+
+//!USANDO CLASES
+class Mascota {
+  constructor(
+    nombreMascota,
+    nombreDueno,
+    cedulaDueno,
+    edadMascota,
+    telefonoDueno,
+    especialidad,
+    medico
+  ) {
+    this.nombreMascota = nombreMascota;
+    this.nombreDueño = nombreDueno;
+    this.cedulaDueño = cedulaDueno;
+    this.edadMascota = edadMascota;
+    this.telefonoDueño = telefonoDueno;
+    this.especialidad = especialidad;
+    this.medico = medico;
+  }
+}
+
+let mascota1 = new Mascota(
+  "Luna",
+  "Ana",
+  "56723982",
+  "6 años",
+  "5675658567",
+  "Farmacología veterinaria",
+  "Juan"
+);
+let mascota2 = new Mascota(
+  "Mystic",
+  "Charlie",
+  "567235896",
+  "7 años",
+  "5678952567",
+  "Farmacología veterinaria",
+  "Daniel"
+);
+let mascota3 = new Mascota(
+  "Negro",
+  "Sebastián",
+  "56722569",
+  "5 años",
+  "5678952567",
+  "Farmacología veterinaria",
+  "Sandra"
+);
+//Arreglo para iterar
+let listaMascotas = [mascota1, mascota2, mascota3];
+
+for (let i = 0; i < listaMascotas.length; i++) {
+  let mascota = listaMascotas[i]; //Traemos cada objeto de cada posicion dentro del array y se almacenan en la variable mascota.
+  //console.log(mascota);
+  //Se crea la fila y columnas
+  let fila = cuerpoTabla.insertRow();
+  let celdaNombreMascota = fila.insertCell();
+  let celdaNombreDueño = fila.insertCell();
+  let celdaCedulaDueño = fila.insertCell();
+  let celdaEdadMascota = fila.insertCell();
+  let celdaTelefonoDueño = fila.insertCell();
+  let celdaEspecialidad = fila.insertCell();
+  let celdaMedico = fila.insertCell();
+  //Se rellenan las celdas con las propiedades de cada objeto sustraido con la variable mascota
+  celdaNombreMascota.textContent = mascota.nombreMascota;
+  celdaNombreDueño.textContent = mascota.nombreDueño;
+  celdaCedulaDueño.textContent = mascota.cedulaDueño;
+  celdaEdadMascota.textContent = mascota.edadMascota;
+  celdaTelefonoDueño.textContent = mascota.telefonoDueño;
+  celdaEspecialidad.textContent = mascota.especialidad;
+  celdaMedico.textContent = mascota.medico;
+}
+
+//!CONSUMO DE UN JSON
+
+fetch(`../data/datos_mascotas.json`)//Solicitud a URL local o externa
+  .then((Response) => Response.json()) //respuesta y conversión a json 
+  .then(data => { //data = objeto convertido ⏫   
+    
+    for (let i = 0; i < data.length; i++) {
+      let mascota = data[i]; //Traemos cada objeto de cada posicion dentro del array y se almacenan en la variable mascota.   
+      
+      //Se crea la fila y columnas
+      let fila = cuerpoTabla.insertRow();
+      let celdaNombreMascota = fila.insertCell();
+      let celdaNombreDueño = fila.insertCell();
+      let celdaCedulaDueño = fila.insertCell();
+      let celdaEdadMascota = fila.insertCell();
+      let celdaTelefonoDueño = fila.insertCell();
+      let celdaEspecialidad = fila.insertCell();
+      let celdaMedico = fila.insertCell();
+      //Se rellenan las celdas con las propiedades de cada objeto sustraido con la variable mascota
+      celdaNombreMascota.textContent = mascota.nombreMascota;
+      celdaNombreDueño.textContent = mascota.nombreDueño;
+      celdaCedulaDueño.textContent = mascota.cedulaDueño;
+      celdaEdadMascota.textContent = mascota.edadMascota;
+      celdaTelefonoDueño.textContent = mascota.telefonoDueño;
+      celdaEspecialidad.textContent = mascota.especialidad;
+      celdaMedico.textContent = mascota.medico;
+    }
+  }).catch(error => console.log("error")); //control de errores
---
+++ Js/medicos.js
@@ -0,0 +1,159 @@
+//Extración de los elementos de la tabala
+let tablaMedicos = document.getElementById("tabla-medicos");
+let cuerpoTabla = tablaMedicos.querySelector("tbody");
+
+//Variables unicas para cada arreglo
+let nombreDoctor = ["Daniel"];
+let apellidoDoctor = ["Bejarano"];
+let cedulaDoctor = ["1022589632"];
+let especialidadDoctor = ["Odontología veterinaria"];
+let consultorioDoctor = ["5"];
+let emailDoctor = ["bejaranito@gmail.com"];
+let numPacientesDoctor = ["5"];
+
+for (let i = 0; i < nombreDoctor.length; i++) {
+  //Se establece una fila
+  let fila = cuerpoTabla.insertRow();
+  //Se establecen los campos de columnas
+  let celdaNombre = fila.insertCell();
+  let celdaApellido = fila.insertCell();
+  let celdaCedula = fila.insertCell();
+  let celdaEspecialidad = fila.insertCell();
+  let celdaConsultorio = fila.insertCell();
+  let celdaEmail = fila.insertCell();
+  let celdaNumPacientes = fila.insertCell();
+  //Se llenan las columnas
+  celdaNombre.textContent = nombreDoctor[i];
+  celdaApellido.textContent = apellidoDoctor[i];
+  celdaCedula.textContent = cedulaDoctor[i];
+  celdaEspecialidad.textContent = especialidadDoctor[i];
+  celdaConsultorio.textContent = consultorioDoctor[i];
+  celdaEmail.textContent = emailDoctor[i];
+  celdaNumPacientes.textContent = numPacientesDoctor[i];
+}
+
+//!USO DE OBJETOS
+//Lista que lamacena los datos para ser recorridos | esta lista también se utiliza para recorrer los objetos creados con la clase.
+const medicos = [
+  {
+    nombreDoctor: "Juan",
+    apellidoDoctor: "Oviedo",
+    cedulaDoctor: "10258963256",
+    especialidadDoctor: "Medicina general",
+    consultorioDoctor: "1",
+    emailDoctor: "juanito@gmail.com",
+    numPacientesDoctor: "2",
+  },
+  {
+    nombreDoctor: "Felipe",
+    apellidoDoctor: "Gonzales",
+    cedulaDoctor: "1056895235",
+    especialidadDoctor: "Medicina interna",
+    consultorioDoctor: "2",
+    emailDoctor: "pipelon@gmail.com",
+    numPacientesDoctor: "10",
+  },
+  {
+    nombreDoctor: "Andrés",
+    apellidoDoctor: "Ramírez",
+    cedulaDoctor: "51258963",
+    especialidadDoctor: "Dermatología veterinaria",
+    consultorioDoctor: "3",
+    emailDoctor: "andy@gmail.com",
+    numPacientesDoctor: "3",
+  },
+];
+
+//!USO DE CLASES
+class Medico {
+  constructor(
+    nombreDoctor,
+    apellidoDoctor,
+    cedulaDoctor,
+    especialidadDoctor,
+    consultorioDoctor,
+    emailDoctor,
+    numPacientesDoctor
+  ) {
+    (this.nombreDoctor = nombreDoctor),
+      (this.apellidoDoctor = apellidoDoctor),
+      (this.cedulaDoctor = cedulaDoctor),
+      (this.especialidadDoctor = especialidadDoctor),
+      (this.consultorioDoctor = consultorioDoctor),
+      (this.emailDoctor = emailDoctor),
+      (this.numPacientesDoctor = numPacientesDoctor);
+  }
+}
+
+let medico1 = new Medico(
+  "Cristian",
+  "Betancourt",
+  "1021012456",
+  "Cirugía veterinaria",
+  "6",
+  "camilo@gmail.com",
+  "6"
+);
+let medico2 = new Medico(
+  "Kevin",
+  "Duran",
+  "102589632",
+  "Odontología veterinaria",
+  "9",
+  "kevincito@gmail.com",
+  "8"
+);
+
+//let medicosList = [medico1, medico2];
+medicos.push(medico1, medico2); //? Método que se utiliza para introducir elementos dentro de un arreglo. Se envian los dos objetos creados por las clases al arreglo que almacena los objetos 🔝, cuando se recorre el arreglo, ya cuenta con las clases.
+
+for (let i = 0; i < medicos.length; i++) {
+  let med = medicos[i]; //Asignación de variable a  posicion del array - objeto para consultar propiedades.
+  //Se establece una fila
+  let fila = cuerpoTabla.insertRow();
+  //Se establecen los campos de columnas
+  let celdaNombre = fila.insertCell();
+  let celdaApellido = fila.insertCell();
+  let celdaCedula = fila.insertCell();
+  let celdaEspecialidad = fila.insertCell();
+  let celdaConsultorio = fila.insertCell();
+  let celdaEmail = fila.insertCell();
+  let celdaNumPacientes = fila.insertCell();
+  //Se llenan las columnas
+  celdaNombre.textContent = med.nombreDoctor;
+  celdaApellido.textContent = med.apellidoDoctor;
+  celdaCedula.textContent = med.cedulaDoctor;
+  celdaEspecialidad.textContent = med.especialidadDoctor;
+  celdaConsultorio.textContent = med.consultorioDoctor;
+  celdaEmail.textContent = med.emailDoctor;
+  celdaNumPacientes.textContent = med.numPacientesDoctor;
+}
+
+//! CONSUMO DE JSON
+
+fetch('../data/datos_medicos.json')
+  .then((Response) => Response.json())
+  .then((data) => {
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      let med = data[i]; //Asignación de variable a  posicion del array - objeto para consultar propiedades.
+      //Se establece una fila
+      let fila = cuerpoTabla.insertRow();
+      //Se establecen los campos de columnas
+      let celdaNombre = fila.insertCell();
+      let celdaApellido = fila.insertCell();
+      let celdaCedula = fila.insertCell();
+      let celdaEspecialidad = fila.insertCell();
+      let celdaConsultorio = fila.insertCell();
+      let celdaEmail = fila.insertCell();
+      let celdaNumPacientes = fila.insertCell();
+      //Se llenan las columnas
+      celdaNombre.textContent = med.nombreDoctor;
+      celdaApellido.textContent = med.apellidoDoctor;
+      celdaCedula.textContent = med.cedulaDoctor;
+      celdaEspecialidad.textContent = med.especialidadDoctor;
+      celdaConsultorio.textContent = med.consultorioDoctor;
+      celdaEmail.textContent = med.emailDoctor;
+      celdaNumPacientes.textContent = med.numPacientesDoctor;
+    }
+  }).catch(error => console.log("error")); //control de errores
